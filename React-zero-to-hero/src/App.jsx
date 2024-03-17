9b07e78c673ad61c61114fb811585afe151126f8
varunsoni3404 <157034960+varunsoni3404@users.noreply.github.com>
2024-03-17 11:07:07 +0530

    Add files via upload

---
+++ React-zero-to-hero/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Card from "./components/Card";
+import Head from "./components/Head";
+import Foot from "./components/Foot";
+function App() {
+  return (
+    <>
+      <Head />
+      <Card />
+      <Foot/>
+    </>
+  );
+}
+
+export default App;
